df0f4c1d9798616eda1f3c5ca04d5621883ed454
Michel Medeiros <michel.tds@gmail.com>
2017-04-10 22:07:43 -0300

    Copiando arquivos

---
+++ config/express.js
@@ -0,0 +1,8 @@
+var express = require('express')
+    ,app = express()
+    ,bodyParser = require('body-parser');
+
+app.use(express.static('./app'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http'),
+    app = require('./config/express');
+
+
+var port = process.env.PORT || 3000;
+http.createServer(app).listen(port, function() {
+    console.log('Servidor estutando na porta: ' + this.address().port);
+});
