be2fdf7af429200e0f272f95cf833843f7046f6a
Michel Medeiros <michel.tds@gmail.com>
2017-04-14 11:46:49 -0300

    New tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
-    clean = require('gulp-clean');
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    htmlReplace = require('gulp-html-replace'),
+    uglify = require('gulp-uglify');
+
+gulp.task('default', ['copy'], function(){
+    gulp.start('build-img', 'build-js', 'build-html');
+});
 
 gulp.task('clean', function() {
     return gulp.src('dist')
@@ -12,8 +19,24 @@ gulp.task('copy', ['clean'], function() {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-img', ['copy'], function() {
+gulp.task('build-img', function() {
     gulp.src('dist/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'));
 });
+
+gulp.task('build-js', function() {
+    gulp.src(['dist/js/jquery.js', 'dist/js/home.js', 'dist/js/produto.js'])
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('build-html', function() {
+    gulp.src('dist/**/*.html')
+        .pipe(htmlReplace({
+            js: 'js/all.js'
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
